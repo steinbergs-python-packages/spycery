e2bbb5adfa836d151e2a36c7446f2b3ad76063db
stephan.steinberg <st.steinberg@t-online.de>
2020-09-27 18:39:58 +0200

    added meta class that acts as a singleton

---
+++ singleton.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""This module provides a metaclass that acts as a singleton."""
+
+import threading
+
+
+class Singleton(type):
+    """A metaclass that acts as a singleton."""
+
+    _instances = {}
+    _lock = threading.Lock()
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            with cls._lock:
+                if cls not in cls._instances:
+                    cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        print("__call__", cls._instances[cls], dir(cls._instances[cls]), args, kwargs)
+        return cls._instances[cls]
+
+
+class SingletonClass(metaclass=Singleton):
+    pass
