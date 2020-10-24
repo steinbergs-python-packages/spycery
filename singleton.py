77f78cf6a1a6bade1c332c1db4ff59fbaf0fd031
stephan.steinberg <st.steinberg@t-online.de>
2020-10-24 18:14:43 +0200

    cleanup

--- singleton.py
+++ singleton.py
@@ -19,7 +19,3 @@ class Singleton(type):
                     cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         # print("__call__", cls._instances[cls], dir(cls._instances[cls]), args, kwargs)
         return cls._instances[cls]
-
-
-class SingletonClass(metaclass=Singleton):
-    pass
