7237303f5b839f00ee3feb5c8db246b608ee25c4
stephan.steinberg <st.steinberg@t-online.de>
2020-10-24 17:57:12 +0200

    disabled print

--- singleton.py
+++ singleton.py
@@ -17,7 +17,7 @@ class Singleton(type):
             with cls._lock:
                 if cls not in cls._instances:
                     cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        print("__call__", cls._instances[cls], dir(cls._instances[cls]), args, kwargs)
+        # print("__call__", cls._instances[cls], dir(cls._instances[cls]), args, kwargs)
         return cls._instances[cls]
 
 
